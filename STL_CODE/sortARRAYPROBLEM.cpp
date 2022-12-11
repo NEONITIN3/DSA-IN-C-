69f6f0243a4787f0fb97ac0c8f206a6ff039c269
Neonitin <nitingaud1999@gmail.com>
2022-12-11 11:19:49 +0530

    STL c++

--- STL_CODE/sortARRAYPROBLEM.cpp
+++ STL_CODE/sortARRAYPROBLEM.cpp
@@ -10,19 +10,22 @@ int main()
 int arr[]={1,100,23,12,87,7,65,17};
 int size=sizeof(arr)/sizeof(arr[0]);
 //sorting in increasing order
-//  sort(arr,arr+size);
 
-// for(int i=0;i<size;i++)
-// cout<<arr[i]<<" ";
+ sort(arr,arr+size);
+ //
+
+for(int i=0;i<size;i++)
+cout<<arr[i]<<" ";
 
 //for sorting particular index range 12,87,7 ko solve karna hai to index 3 pe 12 hai last index
-// sort(arr+3,arr+size);
-// print(arr,size);
+sort(arr+3,arr+size);
+print(arr,size);
 
 //sorting in decending order
 /*
 we using greater() function to sort in descing order 
 */
 sort(arr,arr+size,greater());
+cout<<"sorted in descending order"<<endl;
 print(arr,size);
 }  
