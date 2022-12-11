fee12ebef1469cabca77a0dddacb1dd7370d6a38
Neonitin <nitingaud1999@gmail.com>
2022-12-11 15:00:42 +0530

    STL in c++

--- STL_CODE/numberOFones_count_in_binary.cpp
+++ STL_CODE/numberOFones_count_in_binary.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int count_setbits(int N)
 {  cout<<"this is the simple solution couting set bit 1 in binary"<<endl;
    int cnt=0;
@@ -12,7 +13,8 @@ int count_setbits(int N)
 
    return cnt;
 }
-int main(){
+int main()
+{
 
 //simple way
 
@@ -21,10 +23,14 @@ cout<<count_setbits(7)<<endl;
 
     /* for counting the number of ones in integer 
     we have building function 
-     _builtin_popcount()
+     _builtin_popcount(var);
 
-its count number of one in given 
-inegeter after converting binary
+    its count number of one in given 
+    inegeter after converting binary
     */
+   cout<<"this is the buildin function used for count"<<endl;
+   int m=5;
+   cout<<__builtin_popcount(m);
+   //__builtin_popcount
    
 }
