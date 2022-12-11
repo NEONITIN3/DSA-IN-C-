ce845a6d8eb5d526c0480124f2c43c3761a775ca
Neonitin <nitingaud1999@gmail.com>
2022-12-11 15:06:25 +0530

    STL in c++

--- STL_CODE/numberOFones_count_in_binary.cpp
+++ STL_CODE/numberOFones_count_in_binary.cpp
@@ -23,14 +23,20 @@ cout<<count_setbits(7)<<endl;
 
     /* for counting the number of ones in integer 
     we have building function 
-     _builtin_popcount(var);
+     __builtin_popcount(var);
 
     its count number of one in given 
     inegeter after converting binary
     */
    cout<<"this is the buildin function used for count"<<endl;
    int m=5;
-   cout<<__builtin_popcount(m);
+   cout<<__builtin_popcount(m)<<endl;
    //__builtin_popcount
+
+   //for long long value we used __builtin_popcountll(var)
+
+   cout<<"for long long vlaue"<<endl;
+   long long y=12987654256378923;
+   cout<<__builtin_popcount(y);
    
 }
