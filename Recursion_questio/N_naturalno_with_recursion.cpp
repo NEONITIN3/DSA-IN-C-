aa1b6b58b48e71f17e006d60493aa6083c176040
Neonitin <nitingaud1999@gmail.com>
2022-12-21 09:01:19 +0530

    123321 with recursion

--- Recursion_questio/N_naturalno_with_recursion.cpp
+++ Recursion_questio/N_naturalno_with_recursion.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //printing increasing array with recursion;
 //in 6
-//op 6 5 4 3 2 1 1 2 3 4 5 6;
+//op  1 2 3 4 5 6;
 void solve(int n){
   if(n<1)//base case
   return;
@@ -10,9 +10,21 @@ void solve(int n){
   cout<<n<<" ";
   return;
 }
+//increasing and decresing order
+//5  o/p 5 4 3 2 1 1 2 3 4 5
+
+int inde(int n){
+  cout<<endl;
+  if(n==0)
+  return 0;
+  cout<<n<<" ";
+  inde(n-1);
+  cout<<n<<" ";
+}
 int main()
 {
   int n=5;
   //cin>>n;
   solve( n) ;
+  inde(n);
   }
